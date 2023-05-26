c427d3abc021460420cf33bbc4c012b3006e6fec
Evg <macheev2010@yandex.ru>
2023-05-27 02:50:20 +0300

    Add hello handler test

--- internal/commandhandlers/hello_handler_test.go
+++ internal/commandhandlers/hello_handler_test.go
@@ -1,1 +1,15 @@
 package commandhandlers
+
+import (
+	"testing"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloHandler(t *testing.T) {
+	update := tgbotapi.Update{Message: &tgbotapi.Message{Chat: &tgbotapi.Chat{ID: 1}}}
+	msg := HelloHandler(update)
+
+	assert.Equal(t, msg.Text, "Hello")
+}
